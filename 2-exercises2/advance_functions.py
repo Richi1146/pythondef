f13ba980ca72fa3f79556df146ba658b2f9561c7
Richi1146 <98324008+Richi1146@users.noreply.github.com>
2025-05-07 13:05:39 -0500

    Update advance_functions.py

--- 2-exercises2/advance_functions.py
+++ 2-exercises2/advance_functions.py
@@ -2,7 +2,17 @@ import string
 import random
 
 # 1. Shopping cart simulator
+def calculate_cart_total(cart, iva=0.21):
+    total = sum(item['price'] * item['quantity'] for item in cart)
+    total_with_iva = total * (1 + iva)
+    return total_with_iva
 
+cart = [
+        {"name": "Shirt", "price": 20.0, "quantity": 2},
+        {"name": "Pants", "price": 40.0, "quantity": 1}
+    ]
+total_price = calculate_cart_total(cart)
+print(total_price)
 
 # 2. Text processing (word count)
 
